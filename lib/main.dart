392d82468d5d358fcfcee7ccc6262f0dec085666
spadao <spadao@users.noreply.github.com>
2020-09-16 23:46:00 -0300

    Dice Project Completed
    Added images and formatting
    Added button and functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,49 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            //flex: 2,
+            child: FlatButton(
+              onPressed: () {
+                setDice();
+              },
+              child: Image.asset("images/dice$leftDiceNumber.png"),
+            ),
+          ),
+          Expanded(
+            //flex: 1,
+            child: FlatButton(
+              onPressed: () {
+                setDice();
+              },
+              child: Image.asset("images/dice$rightDiceNumber.png"),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void setDice() {
+    setState(
+      () {
+        rightDiceNumber = Random().nextInt(6) + 1;
+        leftDiceNumber = Random().nextInt(6) + 1;
+      },
+    );
   }
 }
